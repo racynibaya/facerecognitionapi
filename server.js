287662f96de2ad22861d096d998c96cace56d48b
Racyn Ibaya <racynibaya@outlook.com>
2022-11-30 21:14:03 -0500

    converting server to ESModule

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = require('express');
+import express from 'express';
+import bcrypt from 'bcrypt-nodejs';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
-
-app.use(express.json());
 
 const database = {
   users: [
     {
       id: 123,
       email: 'john@email.com',
+      name: 'John',
       password: 'cookies',
       entries: 0,
       joined: new Date(),
@@ -17,6 +19,7 @@ const database = {
     {
       id: 1234,
       email: 'sally@email.com',
+      name: 'Sally',
       password: 'bananas',
       entries: 0,
       joined: new Date(),
@@ -24,16 +27,31 @@ const database = {
   ],
 };
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
+  const { email, password } = req.body;
+
   if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
+    database.users.find(
+      user => user.email === email && user.password === password
+    )
   ) {
-    res.json('success');
+    res.json(
+      database.users[
+        database.users.findIndex(
+          user => user.email === email && user.password === password
+        )
+      ]
+    );
   } else {
     res.status(400).json('error log in');
   }
@@ -41,13 +59,36 @@ app.post('/signin', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  const { email, password, name } = req.body;
+  const { email, name, password } = req.body;
+
+  // bcrypt.hash(password, null, null, function (err, hash) {
+  //   // Store hash in your password DB.
+  //   console.log(hash);
+  // });
+
+  // Load hash from your password DB.
+  bcrypt.compare(
+    'eagle',
+    '$2a$10$JpRozF.9HgluGKMG5qtThuRzbh1ARR1PF.M.2C2Z68MsqA.6MAS5G',
+    function (err, res) {
+      // res == true
+      console.log(res);
+    }
+  );
+  bcrypt.compare(
+    'veggies',
+    '$2a$10$JpRozF.9HgluGKMG5qtThuRzbh1ARR1PF.M.2C2Z68MsqA.6MAS5G',
+    function (err, res) {
+      // res = false
+      console.log(res);
+    }
+  );
 
   database.users.push({
-    id: 12345,
+    id: 125,
     email: email,
+    name: name,
     password: password,
-    name,
     entries: 0,
     joined: new Date(),
   });
@@ -57,9 +98,10 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
+  console.log(typeof id);
   let found = false;
   database.users.forEach(user => {
-    if (user.id === Number(id)) {
+    if (user.id.toString() === id) {
       found = true;
       return res.json(user);
     }
@@ -70,14 +112,22 @@ app.get('/profile/:id', (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('App is running on port 3000');
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === Number(id)) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+
+  if (!found) {
+    return res.status(404).json('Not found!');
+  }
 });
 
-/*
-    -> res = this is working
-    -> signin = POST success / fail
-    -> register = POST = user
-    -> profile/:userId --> GET = user
-    -> image --> PUT --> user
- */
+app.listen(8000, () => {
+  console.log('App is running on port 8000');
+});
