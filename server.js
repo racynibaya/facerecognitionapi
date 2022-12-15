c184507be0df5774f59dff9eaf5f9586337d68af
Racyn Ibaya <racynibaya@outlook.com>
2022-12-15 15:35:28 -0500

    test

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import { getUser } from './controllers/profile.js';
 import { handleRegister } from './controllers/register.js';
 import { handleSignin } from './controllers/signin.js';
 import { users } from './controllers/users.js';
+
 const db = knex({
   client: 'pg',
   version: 15.1,
@@ -17,7 +18,7 @@ const db = knex({
     hostname: process.env.INTERNAL_DATABASE_URL,
     user: 'smart_brain_db_gz76_user',
     port: 5432,
-    password: process.env.DB_PASSWORD,
+    password: `${process.env.DB_PASSWORD}`,
     database: 'smart_brain_db_gz76',
   },
 });
@@ -32,8 +33,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get('/', users(db));
-// app.get('/', (req, res) => res.json('It is working'));
+// app.get('/', users(db));
+app.get('/', (req, res) => res.json('It is working'));
 
 // handleSignin returns a function
 app.post('/signin', handleSignin(db, bcrypt));
